7618de2f89d5be2252575e758a26a8c1165faa81
Ikhizamah Oyinifeoluwakan <jewelhckr@gmail.com>
2023-11-06 12:50:01 +0000

    Finally

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewserviceService } from '../services/newservice.service';
 
 @Component({
   selector: 'app-forms',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./forms.component.css']
 })
 export class FormsComponent {
-
+  constructor (public newservice:NewserviceService){
+    public first_name=""
+    public last_name=""
+    public email=""
+    public password=""
+    public school=""
+  }
 }
